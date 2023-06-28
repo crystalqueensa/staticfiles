8edc8f0d2737588c84940501b09433207c4e6847
KoOnTools <92493805+KoOnTools@users.noreply.github.com>
2023-06-28 09:18:39 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,1 @@
-const keywords = /m=1|xnxx|janda|xxx|xx|sex|seks|porn|naked|telanjang/gi;
-const redirectTo = 'https://www.google.com/';
 
-let m;
-while ((m = keywords.exec(document.location.href)) !== null) {
-    if (m.index === keywords.lastIndex) {
-        keywords.lastIndex++;
-    }
-    m.forEach((match, groupIndex) => {
-        window.location.replace(redirectTo);
-    });
-}
